5dca4dacfce7067ca7a748376c92591563e388bf
Nguyen Huy Hoang <hoangnh0099@gmail.com>
2020-03-10 23:23:49 +0700

    Change Functional to Class

--- src/screens/Home/HomeScreen.js
+++ src/screens/Home/HomeScreen.js
@@ -1,101 +1,115 @@
-import React, { Component, useEffect } from 'react';
-import { FlatList, Text, View, TouchableOpacity, Image, ImageBackground } from 'react-native';
+import React, { Component } from 'react';
+import {
+  FlatList,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  ImageBackground,
+} from 'react-native';
 import styles from './styles';
 import { recipes } from '../../data/dataArrays';
-import MenuImage from '../../components/MenuImage/MenuImage';
 import { ScrollView } from 'react-native-gesture-handler';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
 import firebase from '../../services/FirebaseConfig';
 
-function HomeScreen({ navigation }) {
+class HomeScreen extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-  const onPressRecipe = (item) => {
-    navigation.navigate('RecipeScreen', { item });
+  onPressRecipe = item => {
+    this.navigation.navigate('RecipeScreen', { item });
   };
 
-  function renderRecipes({ item }) {
+  onPressGetStarted() {
+    this.navigation.navigate('TestListScreens');
+  }
+
+  onSignOut = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        navigation.navigate('Login');
+      });
+  };
+
+  renderRecipes({ item }) {
     return (
       <View style={styles.wellcome}>
-        <TouchableOpacity onPress={() => onPressRecipe(item)} >
-          <View style={{ flex: 1, flexDirection: "row" }}>
+        <TouchableOpacity onPress={() => this.onPressRecipe(item)}>
+          <View style={{ flex: 1, flexDirection: 'row' }}>
             <Image style={styles.photo} source={{ uri: item.photo_url }} />
             <Text style={styles.title}>{item.title}</Text>
           </View>
         </TouchableOpacity>
       </View>
-    )
+    );
   }
 
-  function onPressGetStarted() {
-    navigation.navigate("TestListScreens");
-  }
-
-  const onSignOut = () => {
-    firebase.auth().signOut().then(() => {
-      navigation.navigate('Login');
-    });
-  }
-
-  useEffect(() => {
-    const database = firebase.firestore();
-
-    database.collection('chuong1').onSnapshot(querySnapshot => {
-      querySnapshot.forEach(item => console.log(item.data()));
-    })
-  }, [])
-
-  return (
-    <View style={styles.container}>
+  render() {
+    return (
       <View style={styles.container}>
-        <ScrollView style={{ flex: 1 }}>
-          <View style={styles.wellcome1}>
-            <View style={{ flex: 1, paddingLeft: 5 }}>
-              <Text style={{ fontSize: 20, color: "#666666" }}>Hello,</Text>
-              <Text style={{ fontSize: 30, fontWeight: "bold" }}>Tran Dinh Giang</Text>
-            </View>
-            <TouchableOpacity style={styles.headerButtonContainer} onPress={() => navigation.openDrawer()}>
-              <Image
-                style={styles.headerButtonImage}
-                source={require('../../../assets/icons/menu.png')}
-              />
-            </TouchableOpacity>
-          </View>
-
-          <TouchableOpacity onPress={onSignOut}>
-            <Text>Dang Xuat</Text>
-          </TouchableOpacity>
-
-          <View style={styles.rootContainer}>
-            <ImageBackground
-              source={require('../../../assets/backgr.jpg')}        
-              style={styles.backgroundImage}
-              imageStyle={{ borderRadius: 10 }}>
-              <Text style={styles.HomeTitle}>What do you want to test Today?</Text>
-              <TouchableOpacity style={styles.get_started_button} onPress={onPressGetStarted}>
-                <Text style={styles.get_started_text}>Get Started</Text>
+        <View style={styles.container}>
+          <ScrollView style={{ flex: 1 }}>
+            <View style={styles.wellcome1}>
+              <View style={{ flex: 1, paddingLeft: 5 }}>
+                <Text style={{ fontSize: 20, color: '#666666' }}>Hello,</Text>
+                <Text style={{ fontSize: 30, fontWeight: 'bold' }}>
+                  Tran Dinh Giang
+                </Text>
+              </View>
+              <TouchableOpacity
+                style={styles.headerButtonContainer}
+                onPress={() => this.props.navigation.openDrawer()}
+              >
+                <Image
+                  style={styles.headerButtonImage}
+                  source={require('../../../assets/icons/menu.png')}
+                />
               </TouchableOpacity>
-            </ImageBackground>
-          </View>
+            </View>
 
-          <View>
-            <Text style={styles.feedback}>Bài kiểm tra gần đây</Text>
-          </View>
-          <View style={styles.div}>
-            <FlatList
-              vertical
-              showsVerticalScrollIndicator={false}
-              numColumns={1}
-              data={recipes}
-              renderItem={renderRecipes}
-              keyExtractor={item => `${item.recipeId}`}
-            />
-          </View>
-        </ScrollView>
+            <TouchableOpacity onPress={this.onSignOut}>
+              <Text>Dang Xuat</Text>
+            </TouchableOpacity>
+
+            <View style={styles.rootContainer}>
+              <ImageBackground
+                source={require('../../../assets/backgr.jpg')}
+                style={styles.backgroundImage}
+                imageStyle={{ borderRadius: 10 }}
+              >
+                <Text style={styles.HomeTitle}>
+                  What do you want to test Today?
+                </Text>
+                <TouchableOpacity
+                  style={styles.get_started_button}
+                  onPress={this.onPressGetStarted}
+                >
+                  <Text style={styles.get_started_text}>Get Started</Text>
+                </TouchableOpacity>
+              </ImageBackground>
+            </View>
+
+            <View>
+              <Text style={styles.feedback}>Bài kiểm tra gần đây</Text>
+            </View>
+            <View style={styles.div}>
+              <FlatList
+                vertical
+                showsVerticalScrollIndicator={false}
+                numColumns={1}
+                data={recipes}
+                renderItem={this.renderRecipes}
+                keyExtractor={item => `${item.recipeId}`}
+              />
+            </View>
+          </ScrollView>
+        </View>
       </View>
-    </View >
-  );
+    );
+  }
 }
-
 
 export default HomeScreen;
