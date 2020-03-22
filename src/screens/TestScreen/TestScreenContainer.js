c2a133b3eb99e333e1765aa7914c383b1800c747
hieudao500 <hieudao500@gmail.com>
2020-03-22 23:50:16 +0700

    update

--- src/screens/TestScreen/TestScreenContainer.js
+++ src/screens/TestScreen/TestScreenContainer.js
@@ -11,39 +11,6 @@ class TestScreenContainer extends Component {
     };
   }
 
-  // getQuestionCount = async () => {
-  //   try {
-  //     return await firebase
-  //       .database()
-  //       .ref()
-  //       .child('/Chapters/0/CH1/questions')
-  //       .on('value', snap => {
-  //         this.setState({ questionsCount: snap.val() });
-  //       });
-  //   } catch (e) {
-  //     alert(e.message);
-  //   }
-  // };
-
-  // getQuestion = async index => {
-  //   this.setState({ active: index });
-  //   try {
-  //     return await firebase
-  //       .database()
-  //       .ref()
-  //       .child(`/Chapters/0/CH1/questions/${index}`)
-  //       .on('value', snap => {
-  //         this.setState({ questions: snap.val() });
-  //       });
-  //   } catch (e) {
-  //     alert(e.message);
-  //   }
-  // };
-
-  // componentDidMount() {
-  //   this.getQuestionCount().done();
-  // }
-
   render() {
     console.log(this.props.navigation.getParam('question'));
     return (
